00bdc7cc8a2d24b756901c6ac0354e1820c2ea8e
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-08-29 01:38:50 -0400

    on change works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MoneyConverter';
+  currencies: any = ["AUD", "BGN", "BRL", "CAD","CHF","CNY","CZK","DKK","EUR","GBP","HKD","HRK","HUF","IDR","ILS","INR","ISK","JPY","KRW","MXN","MYR","NOK","NZD","PHP","PLN","RON","RUB","SEK","SGD","THB","TRY","USD","ZAR"]
+
+  constructor(public fb: FormBuilder) { }
+
+  originalCurrenciesForm = this.fb.group({
+    name: ['']
+  })
+
+  desiredCurrenciesForm = this.fb.group({
+    name: ['']
+  })
+
+  original_currency: string = ''
+  desired_currency: string = ''
+
+  onSubmitOriginalCurr() {
+    this.original_currency = this.originalCurrenciesForm.value.name;
+  }
+  onSubmitDesiredCurr() {
+    this.desired_currency = this.desiredCurrenciesForm.value.name;
+  }
 }
